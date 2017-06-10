846ce0acb10711c316391621936811f59f4e1e45
Dave Daniels <dave.daniels@lyricfinancial.com>
2017-06-09 20:24:01 -0500

    Apple test

---
+++ src/main/java/com/davedaniels/j8ia/model/Apple.java
@@ -0,0 +1,14 @@
+package com.davedaniels.j8ia.model;
+
+
+public class Apple  {
+    private Integer weight = 0;
+
+    public Apple(Integer weight) {
+        this.weight = weight;
+    }
+    
+    public Integer getWeight() {
+        return weight;
+    }
+}
